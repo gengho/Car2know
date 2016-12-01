d64cf9b9f3d6f7147b36997bd36db5339b0a4fcf
Jason64178 <wxiasen@gmail.com>
2016-11-30 22:41:11 -0800

    Add homework

---
+++ Xiasen's Hw7/QueryData.py
@@ -0,0 +1,65 @@
+from urllib2 import Request, urlopen, URLError
+from datetime import time
+from datetime import date
+import datetime
+import time
+import sys
+import os
+
+
+
+def querydata():
+    request = Request("http://www.car2go.com/api/v2.1/vehicles?loc=Seattle&oauth_consumer_key=Fleet-size&format=json")
+
+    try:
+        response = urlopen(request)
+        return response.read()
+#         raise Exception('spam', 'eggs')
+    except Exception as e:
+        print datetime.datetime.now(), ' Got an error:', str(e)
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno)
+        return ""
+
+def StoreData(interval, path):
+    dt = datetime.datetime.now()
+    tt = dt.timetuple()
+    year = tt[0]
+    month = tt[1]
+    day = tt[2]
+    hour = tt[3]
+    minute = tt[4]
+    second = tt[5]
+    previousDate = str(year) + str(month).zfill(2) + str(day).zfill(2)
+    currentDate = str(year) + str(month).zfill(2) + str(day).zfill(2)
+    
+    f = open(path + currentDate + '.txt','a')
+    
+    while 1:
+        time.sleep(1)
+        dt = datetime.datetime.now()
+        tt = dt.timetuple()
+        year = str(tt[0])
+        month = str(tt[1])
+        day = str(tt[2])
+        hour = str(tt[3])
+        minute = str(tt[4])
+        second = str(tt[5])
+        
+        currentDate = year + month.zfill(2) + day.zfill(2)
+        currentTime = hour.zfill(2) + minute.zfill(2)+ second.zfill(2)
+        
+        if currentDate != previousDate:
+            f = open(path + currentDate + '.txt','a')
+            prviousDate = currentDate
+        
+        if int(second) % interval == 0:
+#             print hour+" "+minute +" "+ second
+            data = querydata()
+            f.write(currentTime + ';' + data +'\n')
+
+if __name__ == '__main__':
+    path = 'F:\\car2godata\\Seattle\\'
+    StoreData(30, path)
+        
---
+++ Xiasen's Hw7/hw7.md
@@ -0,0 +1,2 @@
+this folder is for hw7
+The program is to pull data from Car2go's API and save them in txt file named by date
