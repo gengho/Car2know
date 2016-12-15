c403e1e80581352a0b2aec749c306122d3b32be6
Jason64178 <wxiasen@gmail.com>
2016-12-15 06:37:39 -0800

    Edit __init__.py

--- Car2know/__init__.py
+++ Car2know/__init__.py
@@ -1,1 +1,19 @@
 #init for the project
+
+import shapely
+import geopandas as gpd
+import pandas as pd
+import numpy as np
+import matplotlib
+import pysal
+from scipy import ndimage
+from urllib2 import Request, urlopen, URLError
+from datetime import time
+from datetime import date
+import datetime
+import time
+import sys
+import os
+import matplotlib.pyplot as plt
+%matplotlib inline
+
